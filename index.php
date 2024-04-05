28008e82fef46755b98ed239744bb424117d4365
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-04-05 16:26:38 -0300

    aprendendo sobre ponteiros

--- index.php
+++ index.php
@@ -34,6 +34,28 @@ $result = $mysqli->query("SELECT id, nome FROM user");
 // a função "query" retorna um array
 var_dump($result);
 
+echo " ================ <br>";
+
+// fetch_array -> vai acessar sempre o primeiro elemento da função.
+
+echo "num_row <br>";
+$num_row = $result->fetch_array(MYSQLI_NUM);
+echo " id: " . $num_row[0] . " Name: " . $num_row[1] . "<br>";
+
+var_dump($num_row);
+
+// na segunda chamada ele ajusta o ponteiro interno e chama a proxima linha
+echo "num_assoc <br>";
+$num_assoc = $result->fetch_array(MYSQLI_ASSOC);
+
+var_dump($num_assoc);
+
+// na terceira chamada ele ajusta novamente o ponteiro inteiro e chama a proxima linha
+echo "num_both <br>";
+$num_both = $result->fetch_array(MYSQLI_BOTH);
+
+var_dump($num_both);
+
 // interando sobre o array retornado começando pela última posição para exibi-lo de maneira decrescente na tela
 for($row_no = $result->num_rows -1 ; $row_no >= 0; $row_no--){
     // data_seek -> Busca o resultado no array apartir do id passado

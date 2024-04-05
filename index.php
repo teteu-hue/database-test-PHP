3859e4fffa1ecdb1a1816fab73d912f8941f497e
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-04-05 15:41:23 -0300

    adicionado comentários para ajuda

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$database = "teste";
+$database = "database";
 
-// Create connection
+// create connection
 $mysqli = new mysqli($servername, $username, $password, $database);
 
-// Check connection
+// check connection
 if(!$mysqli){
     die("Connection failed: " . mysqli_connect_error());
 }
@@ -17,30 +17,45 @@ echo "Connected successfully";
 
 echo "<br>";
 
+// rodando uma query SQL
+// criando uma tabela "user"
 $mysqli->query("CREATE TABLE user(id INT, nome VARCHAR(40))");
 
-// Run a SQL query
+// colocando valores na tabela "user"
 $mysqli->query("INSERT INTO user VALUES(1,'Matheus'), (2, 'Jefferson'), (3,'Stella')");
 echo "<pre>";
-//$result = $mysqli->query($sql);
-//var_dump($result);
+
 echo "<br>";
+
 echo "Ordem reversa...\n";
-$result = $mysqli->query("SELECT id, nome FROM user ORDER BY id ASC");
-for($row_no = $result->num_rows - 1; $row_no >= 0; $row_no--){
+// rodando uma query SELECT
+$result = $mysqli->query("SELECT id, nome FROM user");
+
+// a função "query" retorna um array
+var_dump($result);
+
+// interando sobre o array retornado começando pela última posição para exibi-lo de maneira decrescente na tela
+for($row_no = $result->num_rows -1 ; $row_no >= 0; $row_no--){
+    // data_seek -> Busca o resultado no array apartir do id passado
     $result->data_seek($row_no);
+    echo "<br>";
+    // fetch_assoc -> retorna a linha do banco em forma de array
     $row = $result->fetch_assoc();
+    // var_dump($row); // retirar o comentário para obter informações de como a informação retorna
     echo " id: " . $row['id'] . " Name: " . $row['nome'] . "\n";
 }
 
 echo "<br>";
 
 echo "Ordem do conjunto de resultados...\n";
+// interando sobre o array retornado começando da primeira posição até a última
 foreach($result as $row){
     echo " id: " . $row['id'] . " Name: " . $row['nome'] . "\n" ;
 }
 
+// rodando uma query DROP TABLE
 $mysqli->query("DROP TABLE user");
 
+// fechando conexão com o banco de dados
 $mysqli->close();
 ?>
